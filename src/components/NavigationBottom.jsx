6fb042a16f61ef89c73c1b11c12f82f40c136a4e
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-09-18 20:35:00 +0300

    Some changes on "NavigationBottom" component, doing some UI changes.

--- src/components/NavigationBottom.jsx
+++ src/components/NavigationBottom.jsx
@@ -12,28 +12,32 @@ import styling from '../styling'; // Import "styling" variable from "styling.js"
 // whole container, where inside is different navigation links.
 const navigationContainer = StyleSheet.create({
   container: {
+    flex: 1,
     width: '100%',
     justifyContent: 'space-evenly',
     alignItems: 'center',
     flexDirection: 'row',
     backgroundColor: styling.colors.VistaWhite,
     borderColor: styling.colors.Asphalt,
-    borderTopWidth: 2,
+    borderTopWidth: 3,
     height: 50,
     position: 'absolute',
     bottom: 0,
   },
   pressableContainer: {
+    height: '100%',
+    paddingTop: 2,
     flexDirection: 'column',
   },
   iconContainer: {
     textAlign: 'center'
   },
   textContainer: {
-    color: styling.colors.Asphalt,
     fontFamily: styling.fonts.android,
     fontWeight: styling.fontWeights.normal,
     fontSize: styling.fontSizes.default,
+    paddingLeft: 10,
+    paddingRight: 10
   }
 });
 
@@ -44,30 +48,78 @@ const NavigationBottom = () => {
   // Component will render everything inside of (...) back to the user.
   return (
     <View style={navigationContainer.container}>
-      <Pressable style={navigationContainer.pressableContainer}>
-        <Link to="/dashboard">
-          <View>
-            <Feather style={navigationContainer.iconContainer} name="home" size={20} color={styling.colors.Asphalt} />
-            <Text style={navigationContainer.textContainer}>Home</Text>
-          </View>
-        </Link>
-      </Pressable>
-      <Pressable style={navigationContainer.pressableContainer}>
-        <Link to="/dashboard/new-item">
-          <View>
-            <AntDesign style={navigationContainer.iconContainer} name="pluscircleo" size={20} color={styling.colors.Asphalt} />
-            <Text style={navigationContainer.textContainer}>New item</Text>
-          </View>
-        </Link>
-      </Pressable>
-      <Pressable style={navigationContainer.pressableContainer}>
-        <Link to="/dashboard/profile">
-          <View>
-            <AntDesign style={navigationContainer.iconContainer} name="profile" size={20} color={styling.colors.Asphalt} />
-            <Text style={navigationContainer.textContainer}>Profile</Text>
-          </View>
-        </Link>
-      </Pressable>
+      <Link to="/dashboard">
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed
+                ? styling.colors.Asphalt
+                : styling.colors.VistaWhite,
+            },
+            navigationContainer.pressableContainer
+          ]}>
+          {({ pressed }) => (
+            <View>
+              <Feather style={navigationContainer.iconContainer} name="home" size={20} color={ pressed ? styling.colors.VistaWhite : styling.colors.Asphalt } />
+              <Text style={[{
+                color: pressed
+                  ? styling.colors.VistaWhite
+                  : styling.colors.Asphalt,
+                },
+                navigationContainer.textContainer
+              ]}>Home</Text>
+            </View>
+          )}
+        </Pressable>
+      </Link>
+      <Link to="/dashboard/new-item">
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed
+                ? styling.colors.Asphalt
+                : styling.colors.VistaWhite,
+            },
+            navigationContainer.pressableContainer
+          ]}>
+          {({ pressed }) => (
+            <View>
+              <AntDesign style={navigationContainer.iconContainer} name="pluscircleo" size={20} color={ pressed ? styling.colors.VistaWhite : styling.colors.Asphalt } />
+              <Text style={[{
+                color: pressed
+                  ? styling.colors.VistaWhite
+                  : styling.colors.Asphalt
+                },
+                navigationContainer.textContainer
+              ]}>New item</Text>
+            </View>
+          )}
+        </Pressable>
+      </Link>
+      <Link to="/dashboard">
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed
+                ? styling.colors.Asphalt
+                : styling.colors.VistaWhite,
+            },
+            navigationContainer.pressableContainer
+          ]}>
+          {({ pressed }) => (
+            <View>
+              <AntDesign style={navigationContainer.iconContainer} name="profile" size={20} color={ pressed ? styling.colors.VistaWhite : styling.colors.Asphalt } />
+              <Text style={[{
+                color: pressed
+                  ? styling.colors.VistaWhite
+                  : styling.colors.Asphalt,
+                },
+                navigationContainer.textContainer
+              ]}>Profile</Text>
+            </View>
+          )}
+        </Pressable>
+      </Link>
     </View>
   );
 };
